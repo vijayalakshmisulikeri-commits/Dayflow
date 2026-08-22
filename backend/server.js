dd169f7279a5893bb5e25a491ac320408bd22f72
Mrudhula P <mrudhulapreddy3016@gmail.com>
2026-08-22 11:26:12 +0530

    chore: connected backend to MongoDB Atlas

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,18 @@
 const express = require('express');
-const mongoose = require('mongoose');
 const authRoutes = require('../routes/auth');
 
 
 const app = express();
 app.use(express.json());
 
-mongoose.connect('mongodb://localhost:27017/hrms');
+const mongoose = require("mongoose");
+
+mongoose.connect(
+  "mongodb+srv://mrudhulareddy3016_db_user:Y0v2FEFs2tUbgrH4@dayflowcluster.bthnlss.mongodb.net/hrms?retryWrites=true&w=majority&appName=DayflowCluster"
+)
+.then(() => console.log("✅ Connected to MongoDB Atlas"))
+.catch(err => console.error("❌ MongoDB connection error:", err));
+
 
 app.use('/auth', authRoutes);
 
